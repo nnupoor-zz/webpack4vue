096e2ec9753f7621aa7b20c2aa2e88378546ffc9
nnupoor <nupoor.neha@gmail.com>
2018-10-05 10:31:02 +0530

    fixed path for test route

--- src/tests/HelloWebpack.spec.js
+++ src/tests/HelloWebpack.spec.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 // Taken from https://vuejs.org/v2/guide/unit-testing.html
 // The path is relative to the project root.
-import MyComponent from '../../pages/HelloWebpack.vue';
+import MyComponent from './../pages/HelloWebpack.vue';
 
 describe('MyComponent', () => {
   it(`should set correct welcomeText value`, () => {
@@ -13,7 +13,7 @@ describe('MyComponent', () => {
     it('sets the correct default data', () => {
       expect(typeof MyComponent.data).toBe('function')
       const defaultData = MyComponent.data()
-      expect(defaultData.welcomeText).toBe( 'Welcome, old friend!')
+      expect(defaultData.welcomeText).toBe('Welcome, old friend!')
     })
 
     it('renders the correct message', () => {
